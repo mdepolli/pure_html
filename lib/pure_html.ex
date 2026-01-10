b7281a226fc148f182c64391276680bec4fda234
Marcelo De Polli <marcelo.polli@gmail.com>
2026-01-09 23:53:13 -0300

    Add PureHTML.parse/1 convenience function

--- lib/pure_html.ex
+++ lib/pure_html.ex
@@ -2,4 +2,26 @@ defmodule PureHTML do
   @moduledoc """
   HTML parsing library.
   """
+
+  alias PureHTML.{Tokenizer, TreeBuilder}
+
+  @doc """
+  Parses an HTML string into a document tree.
+
+  Returns `{doctype, nodes}` where:
+  - `doctype` is `nil` or a doctype tuple
+  - `nodes` is a list of `{tag, attrs, children}` tuples
+
+  ## Examples
+
+      iex> PureHTML.parse("<p>Hello</p>")
+      {nil, [{"html", %{}, [{"head", %{}, []}, {"body", %{}, [{"p", %{}, ["Hello"]}]}]}]}
+
+  """
+  def parse(html) when is_binary(html) do
+    html
+    |> Tokenizer.tokenize()
+    |> Enum.to_list()
+    |> TreeBuilder.build()
+  end
 end
